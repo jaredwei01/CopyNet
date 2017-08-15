7cfe954d95d6298f6698aca1393b36be13bcb166
Halfish <zhangxb.sysu@gmail.com>
2017-08-15 15:59:16 +0800

    add train, eval, inference mode
    Change-Id: I18a36bb00b9e82f8c3fa5f5a36c0cafd21021a4c

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ config = Config('parameters.json')
 
 # prepare data
 vocab = data_util.Vocabulary()
-vocab.build_from_file('small.txt')
-dataset = data_util.Dataset(vocab, config)
+vocab.build_from_file('train.txt')
+train_data = data_util.Dataset(vocab, config.copy(dataset_filepath='train.txt'))
+eval_data = data_util.Dataset(vocab, config.copy(dataset_filepath='eval.txt'))
+infer_data = data_util.Dataset(vocab, config.copy(dataset_filepath='infer.txt'))
 
 # placeholder for inputs
 encoder_inputs = tf.placeholder(tf.int32, shape=(config.batch_size, None))
@@ -21,7 +23,8 @@ encoder_inputs_lengths = tf.placeholder(tf.int32, shape=(config.batch_size,))
 decoder_inputs_lengths = tf.placeholder(tf.int32, shape=(config.batch_size,))
 
 
-def build_model():
+def build_model(mode='train'):
+    assert mode in {'train', 'eval', 'infer'}, 'invalid mode!'
     # embedding maxtrix
     embedding_matrix = tf.get_variable("embedding_matrix", [
         vocab.size, config.embedding_size])
@@ -47,10 +50,30 @@ def build_model():
                         decoder_cell, 
                         attention_mechanism, 
                         attention_layer_size=config.encoder_hidden_size)
+    decoder_initial_state = decoder_cell.zero_state(config.batch_size, 
+                                                dtype=tf.float32)
 
     # decoder
-    decoder_initial_state = decoder_cell.zero_state(config.batch_size, 
+    if mode == 'infer':
+        decoder_initial_state = decoder_cell.zero_state(config.batch_size, 
                                                     dtype=tf.float32)
+        helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(
+                    embedding_matrix, 
+                    tf.fill([config.batch_size], vocab.word2id[u"START"]),
+                    vocab.word2id[u"END"])
+        projection_layer = layers_core.Dense(vocab.size, use_bias=False)
+        decoder = tf.contrib.seq2seq.BasicDecoder(decoder_cell,
+                                                  helper,
+                                                  decoder_initial_state,
+                                                  output_layer=projection_layer)
+        maximum_iterations = tf.round(tf.reduce_max(encoder_inputs_lengths) * 2)
+        final_outputs, final_state, seq_len = \
+                tf.contrib.seq2seq.dynamic_decode(decoder,
+                                    maximum_iterations=maximum_iterations)
+        translations = final_outputs.sample_id
+        return translations
+
+    # train or eval mode
     helper = tf.contrib.seq2seq.TrainingHelper(decoder_inputs_emb, 
                                                decoder_inputs_lengths, 
                                                time_major=False)
@@ -72,38 +95,83 @@ def build_model():
     loss = tf.reduce_sum(crossent * target_weights / tf.to_float(
         config.batch_size))
 
+    if mode == 'eval':
+        return loss
+
     # gradient clip
     params = tf.trainable_variables()
     gradients = tf.gradients(loss, params)
     clipped_gradients, _ = tf.clip_by_global_norm(gradients,
                                                   config.max_grad_norm)
-    optimizer = tf.train.AdamOptimizer(
-        learning_rate=config.learning_rate)
+    optimizer = tf.train.AdamOptimizer(learning_rate=config.learning_rate)
     train_op = optimizer.apply_gradients(zip(clipped_gradients, params))
 
     return loss, train_op
 
 
 def train():
-    fetches = build_model()
+    with tf.variable_scope('root'):
+        train_loss, train_op = build_model(mode='train')
+
+    with tf.variable_scope('root', reuse=True):
+        eval_loss = build_model(mode='eval')
+
+    with tf.variable_scope('root', reuse=True):
+        translations = build_model(mode='infer')
 
     # session configure for GPU
     sess_config = tf.ConfigProto()
     # sess_config.gpu_options.allow_growth = True
     # sess_config.per_process_gpu_memory_fraction = 0.5
     with tf.Session(config=sess_config) as sess:
+        saver = tf.train.Saver()
         sess.run(tf.global_variables_initializer())
-        for epoch in range(config.max_epoch):
-            batch = dataset.next_batch()
+        for batchid in range(config.max_batchid):
+            # training
+            train_batch = train_data.next_batch()
             feed_dict = {
-                encoder_inputs: batch.encoder_inputs,
-                decoder_inputs: batch.decoder_inputs,
-                decoder_outputs: batch.decoder_outputs,
-                encoder_inputs_lengths: batch.encoder_inputs_lengths,
-                decoder_inputs_lengths: batch.decoder_inputs_lengths
+                encoder_inputs: train_batch.encoder_inputs,
+                decoder_inputs: train_batch.decoder_inputs,
+                decoder_outputs: train_batch.decoder_outputs,
+                encoder_inputs_lengths: train_batch.encoder_inputs_lengths,
+                decoder_inputs_lengths: train_batch.decoder_inputs_lengths
             }
-            loss, _ = sess.run(fetches, feed_dict=feed_dict)
-            print 'epoch = %d, loss = %f' % (epoch, loss)
+            loss_train, _ = sess.run([train_loss, train_op], feed_dict=feed_dict)
+            print 'batchid = %d, train loss = %f' % (batchid, loss_train)
+
+            # save checkpoints
+            if (batchid + 1) % config.save_freq == 0:
+                saver.save(sess, './', global_step=batchid)
+                print 'model saved'
+
+            # eval
+            if (batchid + 1) % config.eval_freq == 0:
+                eval_batch = eval_data.next_batch()
+                feed_dict = {
+                    encoder_inputs: eval_batch.encoder_inputs,
+                    decoder_inputs: eval_batch.decoder_inputs,
+                    decoder_outputs: eval_batch.decoder_outputs,
+                    encoder_inputs_lengths: eval_batch.encoder_inputs_lengths,
+                    decoder_inputs_lengths: eval_batch.decoder_inputs_lengths
+                }
+                loss_eval = sess.run(eval_loss, feed_dict=feed_dict)
+                print '\t\tbatchid = %d, eval loss = %f' % (batchid, loss_eval)
+
+            # inference
+            if (batchid + 1) % config.infer_freq == 0:
+                infer_batch = infer_data.next_batch()
+                feed_dict = {
+
+                    encoder_inputs: eval_batch.encoder_inputs,
+                    decoder_inputs: eval_batch.decoder_inputs,
+                    decoder_outputs: eval_batch.decoder_outputs,
+                    encoder_inputs_lengths: eval_batch.encoder_inputs_lengths,
+                    decoder_inputs_lengths: eval_batch.decoder_inputs_lengths
+                }
+                word_ids = sess.run(translations, feed_dict=feed_dict)
+                print '\nbatchid = %d, translations = %s' % (batchid, word_ids)
+                print word_ids.shape
+                print type(word_ids)
 
 
 if __name__ == '__main__':
